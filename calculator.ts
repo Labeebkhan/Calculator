dfdac848f5440df437d385730629b808c8947b97
Labeebkhan <labeebkhan7766@gmail.com>
2024-02-18 17:46:38 +0500

    Changes made and new code is written

---
+++ calculator.ts
@@ -0,0 +1,83 @@
+import inquirer, { Answers, QuestionCollection } from "inquirer";
+import chalk from "chalk";
+
+
+let callculator_items:QuestionCollection = [{
+    name : "num1", // This the Question on the prompt screen also with this we have to access in .then operation
+    type : "number",
+    message: chalk.greenBright("First Number = "),
+    
+},
+{
+    name : "num2",// This the Question on the prompt screen also with this we have to access in .then operation
+    type : "number",
+    message : chalk.bgRedBright("Your Second = "),
+},
+{
+    name : "operation",
+    type: "list",
+    message:  chalk.yellowBright("Choosed operation = "),
+    choices : ["+","-","*","/"]
+}]
+
+ var answeer : Promise<Answers> = inquirer.prompt(callculator_items);
+
+ answeer.then((answeer:Answers)=>{
+    switch (answeer.operation){
+        case "+":
+            console.log(chalk.bgBlueBright("The result of Addition ",answeer.num1 + answeer.num2));
+            break;
+        case "-":
+            console.log(chalk.bgRedBright("The result of  Subtraction is ",answeer.num1-answeer.num2));
+            break;
+
+        case "*":
+            console.log( chalk.bgGreenBright("The result of Multiplication is ", answeer.num1*answeer.num2));
+            break;
+
+        case "/":
+            console.log(chalk.bgCyanBright("The result of Divison is ", answeer.num1/answeer.num2));
+            break;
+            default:
+                console.log("Wrong Operation");
+    } 
+ })
+ .catch((err)=>{
+    console.log(`There is Something Error in the Code ${err}`);
+ })
+
+
+
+
+
+//Another Work for me to rethink my collective work
+//Simple Menu Accepter Pizza
+const select : QuestionCollection = [{
+    name: 'Order',
+    type: 'i',
+    message: "Please Enter Your Order = "
+},
+{
+    name : "Size",
+    type: 'list',
+    choices:['Small','Medium','Large','Jumbo'],
+    message: `You've Order Pizza of Size = `
+},
+{
+    name :'Drinks',
+    type: 'list',
+    choices:['Pepsi','Fanta','Coca Cola','Mountain Dew'],
+    message: 'Drinks? = '
+}
+]
+
+var answeer : Promise<Answers> = inquirer.prompt(select);
+
+ answeer.then((answeer:Answers)=>{
+    console.log("You Have Selected ",answeer);
+    console.log(chalk.greenBright,"Youre Order Is Successfully Placed");
+ })
+
+ .catch((err)=>{
+    console.log(`There is Something Error in the Code ${err}`);
+ })
